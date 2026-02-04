4c49a4b2ba9c898021f39fa795b458563f8bb540
Tom Cripps <tom.cripps@port.ac.uk>
2026-02-04 11:47:12 +0000

    bump version to 2026020401

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'aiplacement_modgen';
 $plugin->release = '0.3.0';
-$plugin->version = 2026012300;
+$plugin->version = 2026020401;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;

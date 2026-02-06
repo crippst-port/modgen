ceeebf3d27f0a318ea60a68e882c95664bfd285f
Tom Cripps <tom.cripps@port.ac.uk>
2026-02-06 11:27:03 +0000

    fix test security

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'aiplacement_modgen';
-$plugin->release = '0.3.0';
-$plugin->version = 2026020403;
+$plugin->release = '0.4.0';
+$plugin->version = 2026020600;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
